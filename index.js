037afcb1c3a884212cd97b515acf6a60a2d30e9e
Boris Kubiak <kubiakboris@gmail.com>
2017-07-19 08:32:52 +0200

    Add interpolation options

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ const tinygradient = require('tinygradient');
 
 function InitGradient() {
 	const grad = tinygradient.apply(this, arguments);
-	return str => applyGradient(str.toString(), grad);
+	return (str, opts) => applyGradient(str.toString(), grad, opts);
 }
 
-function applyGradient(str, gradient) {
-	const colors = gradient.rgb(str.length);
+function applyGradient(str, gradient, opts) {
+	const options = validateOptions(opts);
+	const colors = options.interpolation.toLowerCase() === 'hsv' ? gradient.hsv(str.length, options.hsvSpin) : gradient.rgb(str.length);
 	let result = '';
 	for (let i = 0, len = str.length; i < len; i++) {
 		result += chalk.hex(colors[i].toHex())(str[i]);
 	}
 	return result;
+}
+
+function validateOptions(opts) {
+	const options = Object.assign({interpolation: 'rgb', hsvSpin: 'short'}, opts);
+	if (opts !== undefined && typeof opts !== 'object') {
+		throw new TypeError(`Expected \`options\` to be an \`object\`, got \`${typeof opts}\``);
+	}
+
+	if (typeof options.interpolation !== 'string') {
+		throw new TypeError(`Expected \`options.interpolation\` to be a \`string\`, got \`${typeof options.interpolation}\``);
+	}
+
+	if (options.interpolation.toLowerCase() === 'hsv' && typeof options.hsvSpin !== 'string') {
+		throw new TypeError(`Expected \`options.hsvSpin\` to be a \`string\`, got \`${typeof options.hsvSpin}\``);
+	}
+	return options;
 }
 
 module.exports = InitGradient;
--- test.js
+++ test.js
@@ -6,6 +6,25 @@ test('throw error if wrong gradient arguments', t => {
 	t.throws(() => g('red')('abc'));
 });
 
+test('throw error if options is not an object', t => {
+	t.throws(() => g('blue', 'red')('abc', false), 'Expected `options` to be an `object`, got `boolean`');
+});
+
+test('throw error if interpolation is not a string', t => {
+	t.throws(() => g('blue', 'red')('abc', {interpolation: 1000}), 'Expected `options.interpolation` to be a `string`, got `number`');
+});
+
+test('throw error if hsvSpin is not a string, but only if interpolation is HSV', t => {
+	t.notThrows(() => g('blue', 'red')('abc', {hsvSpin: 42}));
+	t.throws(() => g('blue', 'red')('abc', {interpolation: 'hsv', hsvSpin: 42}), 'Expected `options.hsvSpin` to be a `string`, got `number`');
+});
+
 test('works fine', t => {
 	t.is(g('blue', 'white', 'red')('abc'), '\u001b[94ma\u001b[39m\u001b[97mb\u001b[39m\u001b[91mc\u001b[39m');
+
+	t.is(g('red', 'green')('abc', {interpolation: 'hsv'}),
+        '\u001b[91ma\u001b[39m\u001b[33mb\u001b[39m\u001b[32mc\u001b[39m'); // Red -> yellow -> green (short arc)
+
+	t.is(g('red', 'green')('abc', {interpolation: 'hsv', hsvSpin: 'long'}),
+        '\u001b[91ma\u001b[39m\u001b[34mb\u001b[39m\u001b[32mc\u001b[39m'); // Red -> blue -> green (long arc)
 });
