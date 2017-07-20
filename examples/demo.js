ec7ba69a398316aec4f8ce36f5f3142f90080f2a
Boris Kubiak <kubiakboris@gmail.com>
2017-07-20 21:44:37 +0200

    Make a better demo

--- examples/demo.js
+++ examples/demo.js
@@ -2,7 +2,12 @@
 const gradient = require('..');
 
 const log = console.log;
-const str = '   Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+const str = '   	Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+const title = Buffer.from('DQogICAgICAgICAgICAgICAgICAgICAgXyBfICAgICAgICAgICAgXyAgICAgICAgICAgIF8gICAgICAgIF8gICAgICAgICAgICAgDQogICBfXyBfIF8gX18gX18gXyAgX198IChfKSBfX18gXyBfXyB8IHxfICAgICAgX19ffCB8XyBfIF9fKF8pXyBfXyAgIF9fIF8gDQogIC8gX2AgfCAnX18vIF9gIHwvIF9gIHwgfC8gXyBcICdfIFx8IF9ffF9fX18vIF9ffCBfX3wgJ19ffCB8ICdfIFwgLyBfYCB8DQogfCAoX3wgfCB8IHwgKF98IHwgKF98IHwgfCAgX18vIHwgfCB8IHx8X19fX19cX18gXCB8X3wgfCAgfCB8IHwgfCB8IChffCB8DQogIFxfXywgfF98ICBcX18sX3xcX18sX3xffFxfX198X3wgfF98XF9ffCAgICB8X19fL1xfX3xffCAgfF98X3wgfF98XF9fLCB8DQogIHxfX18vICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgfF9fXy8gDQo=', 'base64').toString();
+
+for (const l of title.split('\n')) {
+	log(gradient.pastel(l.replace(/\s/g, 'i')).replace(/i/g, ' '));
+}
 
 log('');
 
