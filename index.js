fca9483d01154d0561b5bac98c5904f561ef35b5
Boris Kubiak <kubiakboris@gmail.com>
2017-10-27 18:56:53 +0200

    Node 4 compatibility

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function applyGradient(str, gradient, opts) {
 function multilineGradient(str, gradient, opts) {
 	const options = validateOptions(opts);
 	const lines = str.split('\n');
-	const maxLength = Math.max(gradient.stops.length, ...(lines.map(l => l.length)));
+	const maxLength = Math.max.apply(null, lines.map(l => l.length).concat([gradient.stops.length]));
 	const colors = getColors(gradient, options, maxLength);
 	const results = [];
 	for (const line of lines) {
